5bbc2ab75efa9e090652bafb77934ebb0adfbd64
ehsanghannad <ghannad73@gmail.com>
2025-06-24 17:54:51 +0330

    style(index.css): add color var feat(components): add SellBtn component

---
+++ Trading Platform/src/Components/Share/Btn/SellBtn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SellBtn() {
+  return (
+    <>
+      <div className="bg-btnRed text-textWhite flex h-8 w-[216px] items-center justify-center rounded-[3px] text-xs font-bold">
+        Sell BTC
+      </div>
+    </>
+  );
+}
--- Trading Platform/src/Pages/Index.jsx
+++ Trading Platform/src/Pages/Index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BuyBtn from "../Components/Share/Btn/BuyBtn";
+import SellBtn from "../Components/Share/Btn/SellBtn";
 
 export default function Index() {
   return (
     <>
       <BuyBtn />
+      <SellBtn />
     </>
   );
 }

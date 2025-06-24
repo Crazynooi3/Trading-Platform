fb25d70ffc9b32fcc243db3c5e9d421a545d2c4a
ehsanghannad <ghannad73@gmail.com>
2025-06-24 17:45:30 +0330

    style(index.css): fix them var color

---
+++ Trading Platform/src/Components/Share/Btn/BuyBtn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function BuyBtn() {
+  return (
+    <>
+      <div className="bg-btnGreen text-textWhite">Buy BTC</div>
+    </>
+  );
+}
--- Trading Platform/src/Pages/Index.jsx
+++ Trading Platform/src/Pages/Index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import BuyBtn from "../Components/Share/Btn/BuyBtn";
 
 export default function Index() {
-  return <div>Index</div>;
+  return (
+    <>
+      <BuyBtn />
+    </>
+  );
 }

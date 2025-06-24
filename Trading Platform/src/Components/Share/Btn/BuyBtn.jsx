a4caf260ea7138630137a526d5bb68676a1df154
ehsanghannad <ghannad73@gmail.com>
2025-06-24 17:55:37 +0330

    style: bold to semibold

--- Trading Platform/src/Components/Share/Btn/BuyBtn.jsx
+++ Trading Platform/src/Components/Share/Btn/BuyBtn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BuyBtn() {
   return (
     <>
-      <div className="bg-btnGreen text-textWhite flex h-8 w-[216px] items-center justify-center rounded-[3px] text-xs font-bold">
+      <div className="bg-btnGreen text-textWhite flex h-8 w-[216px] items-center justify-center rounded-[3px] text-xs font-semibold">
         Buy BTC
       </div>
     </>

533ebb92ea932cd69805a61e939b9dde4b881037
ehsanghannad <ghannad73@gmail.com>
2025-07-05 18:16:11 +0330

    Create: [Component]  Notice.jsx

--- Trading Platform/src/Pages/Index.jsx
+++ Trading Platform/src/Pages/Index.jsx
@@ -1,11 +1,13 @@
 import FuturesMainContent from "../Components/base/FuturesMainContent/FuturesMainContent";
 import Header from "../Components/Share/Header/Header";
+import Notice from "../Components/Share/Notice/Notice";
 import TabTextWrapper from "../Components/Share/Tab/TabTextWrapper";
 
 export default function Index() {
   return (
     <>
       <Header />
+      <Notice />
       <FuturesMainContent />
       <div className="p-20">
         <TabTextWrapper />

5b1f6c51d7b84ed05c9f063cfd183b758ed5f131
ehsanghannad <ghannad73@gmail.com>
2025-07-05 19:04:58 +0330

    fix: [Style] ElTabs px removed

--- Trading Platform/src/Components/Share/Tab/ElTabs.jsx
+++ Trading Platform/src/Components/Share/Tab/ElTabs.jsx
@@ -5,10 +5,10 @@ export default function ElTabs() {
     <div className="border-border-border1 flex h-10 items-center justify-between border-b px-4">
       <div className="flex h-full w-full items-center justify-between">
         <div className="flex h-full items-center">
-          <div className="text-text-text1 mx-0.5 flex h-full items-center justify-center border-b-2 px-1.5 font-medium">
+          <div className="text-text-text1 mr-5 flex h-full items-center justify-center border-b-2 font-medium">
             <span>Trade</span>
           </div>
-          <div className="text-text-text3 mx-0.5 flex h-full items-center justify-center px-1.5 font-medium">
+          <div className="text-text-text3 flex h-full items-center justify-center font-medium">
             <span>Tools</span>
           </div>
         </div>

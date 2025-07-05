284f5745b71e380ce0133ce4b554e077ce91fe34
ehsanghannad <ghannad73@gmail.com>
2025-07-05 17:39:41 +0330

    nothing

--- Trading Platform/src/Components/base/FuturesMainContent/FuturesMainContent.jsx
+++ Trading Platform/src/Components/base/FuturesMainContent/FuturesMainContent.jsx
@@ -3,10 +3,10 @@ import ElTabs from "../../Share/Tab/ElTabs";
 
 export default function FuturesMainContent() {
   return (
-    <div className="grid h-screen grid-cols-6">
-      <div className="col-span-4 text-white">1</div>
-      <div className="col-span-1 text-white">2</div>
-      <div className="border-border-border1 col-span-1 border-l text-white">
+    <div className="grid h-screen grid-cols-10">
+      <div className="col-span-6 text-white">1</div>
+      <div className="col-span-2 text-white">2</div>
+      <div className="border-border-border1 col-span-2 border-l text-white">
         <ElTabs />
       </div>
     </div>

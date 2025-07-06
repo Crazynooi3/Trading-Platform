3f235df859b718b058a3ab35d8f4a0912d12267f
ehsanghannad <ghannad73@gmail.com>
2025-07-06 22:24:12 +0330

    Create: [Component] Orderbook.jsx

--- Trading Platform/src/Components/base/FuturesMainContent/FuturesMainContent.jsx
+++ Trading Platform/src/Components/base/FuturesMainContent/FuturesMainContent.jsx
@@ -4,12 +4,15 @@ import TabTextWrapper from "../../Share/Tab/TabTextWrapper";
 import FuturesMarginTabs from "../../Share/Tab/FuturesMarginTabs";
 import OrderPlace from "../Ordering/OrderPlace";
 import SpotSlider from "../../Share/Sliders/SpotSlider";
+import OrderBook from "../OrderBook/OrderBook";
 
 export default function FuturesMainContent() {
   return (
-    <div className="grid h-screen grid-cols-10">
+    <div className="grid h-lvh grid-cols-10">
       <div className="col-span-6 text-white">1</div>
-      <div className="col-span-2 text-white">2</div>
+      <div className="col-span-2 text-white">
+        <OrderBook />
+      </div>
       <div className="border-border-border1 col-span-2 border-l text-white">
         <ElTabs />
         <FuturesMarginTabs />
---
+++ Trading Platform/src/Components/base/OrderBook/OrderBook.jsx
@@ -0,0 +1,289 @@
+import React from "react";
+
+export default function OrderBook() {
+  return (
+    <div className="mt-1 h-full">
+      {/* Sell part */}
+      <div className="h-1/2">
+        <ul className="flex h-full w-full flex-col items-center justify-end overflow-scroll">
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex h-5 w-full items-center justify-between px-4">
+            <span className="text-danger-danger1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+        </ul>
+      </div>
+      {/* LastPrice */}
+      <div className="flex h-10 w-full items-center px-4">
+        <span className="text-success-success1 mr-2 text-lg">2.2571</span>
+        <span className="text-text-text4 text-sm underline">2.2571</span>
+      </div>
+      {/* Buy part */}
+      <div className="h-1/2">
+        <ul className="flex h-full w-full flex-col justify-start overflow-scroll">
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+          <li className="flex w-full items-center justify-between px-4">
+            <span className="text-success-success1 w-full text-start text-xs">
+              2.2764
+            </span>
+            <span className="w-full text-end text-xs">69.54K</span>
+            <span className="w-full text-end text-xs">69.54K</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}

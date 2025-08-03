de0699ebb4d89b77a2e74e631f45f0199bd2d6b4
Ehsan ... <ghannad73@gmail.com>
2025-08-03 19:06:40 +0330

    fix : add Span

--- Trading Platform/src/Components/base/Ordering/TPSL.jsx
+++ Trading Platform/src/Components/base/Ordering/TPSL.jsx
@@ -3,7 +3,7 @@ import OrderInput from "../../Share/Input/OrderInput";
 export default function TPSL() {
   return (
     <div className="border-border-border1 h-min-8 mx-4 my-3 flex items-start border-t border-b">
-      <div className="flex w-full flex-wrap items-center space-x-2 py-2">
+      <div className="relative flex w-full flex-wrap items-center space-x-2 py-2">
         <input
           type="checkbox"
           id="TP/SL"
@@ -11,11 +11,14 @@ export default function TPSL() {
         />
         <label
           htmlFor="TP/SL"
-          className="text-xs"
+          className="flex justify-between text-xs"
           style={{ width: "calc(100% - 2rem)" }}
         >
           TP/SL
         </label>
+        <span className="text-primary-primary3 absolute top-0 right-0 pt-2 text-xs">
+          Advanced
+        </span>
 
         <div className="mt-4 hidden w-full peer-checked:block">
           <OrderInput text="Take Profit" detail="Lastest" />

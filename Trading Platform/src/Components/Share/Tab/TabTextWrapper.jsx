2e7b112b35ac4b610fcbb60a32f85f4beba9970a
Ehsan ... <ghannad73@gmail.com>
2025-09-14 18:08:28 +0330

    style

--- Trading Platform/src/Components/Share/Tab/TabTextWrapper.jsx
+++ Trading Platform/src/Components/Share/Tab/TabTextWrapper.jsx
@@ -6,7 +6,7 @@ export default function TabTextWrapper() {
       {/* Open Tab */}
       <div className="relative h-10 w-full">
         <div className="bg-success-success1 h-full w-[80%] rounded-tl-lg rounded-bl-lg"></div>
-        <div className="bg-success-success1 absolute top-0 right-1 h-full w-[47px] -skew-x-[20deg] rounded-tr-lg rounded-br-lg"></div>
+        <div className="bg-success-success1 absolute top-0 right-1 h-full w-[50%] -skew-x-[20deg] rounded-tr-lg rounded-br-lg"></div>
         <div className="absolute top-0 flex h-full w-full items-center justify-center text-center">
           <span className="font-semibold text-white">Open</span>
         </div>
@@ -14,7 +14,7 @@ export default function TabTextWrapper() {
       {/* Close Tab */}
       <div className="relative h-10 w-full">
         <div className="bg-danger-danger1 absolute top-0 right-0 h-full w-[80%] rounded-tr-lg rounded-br-lg"></div>
-        <div className="bg-danger-danger1 absolute top-0 left-0 h-full w-[47px] -skew-x-[20deg] rounded-tl-lg rounded-bl-lg"></div>
+        <div className="bg-danger-danger1 absolute top-0 left-0 h-full w-[50%] -skew-x-[20deg] rounded-tl-lg rounded-bl-lg"></div>
         <div className="absolute top-0 flex h-full w-full items-center justify-center text-center">
           <span className="font-semibold text-white">Close</span>
         </div>

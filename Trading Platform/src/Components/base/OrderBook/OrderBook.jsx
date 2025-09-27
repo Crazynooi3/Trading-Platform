e33dfe86ea8eee228c99604cfd033b8202ac3992
Ehsan ... <ghannad73@gmail.com>
2025-09-27 17:34:36 +0330

    edite one comment

--- Trading Platform/src/Components/base/OrderBook/OrderBook.jsx
+++ Trading Platform/src/Components/base/OrderBook/OrderBook.jsx
@@ -135,7 +135,7 @@ export default function OrderBook() {
     onError: (error) => console.error("WebSocket error:", error),
   });
 
-  // WebSocket
+  // WebSocket Begining MSG
   useEffect(() => {
     sendMessage(
       JSON.stringify({

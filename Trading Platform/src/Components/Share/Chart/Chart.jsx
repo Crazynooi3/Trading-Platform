09ba4c234747f542e4b29d1546557aca498c34bb
Ehsan ... <ghannad73@gmail.com>
2025-09-30 18:18:22 +0330

    fix : currency show when change symbol

--- Trading Platform/src/Components/Share/Chart/Chart.jsx
+++ Trading Platform/src/Components/Share/Chart/Chart.jsx
@@ -1,12 +1,25 @@
 import { AdvancedChart } from "react-tradingview-embed";
+import { useParams } from "react-router-dom";
+const Chart = () => {
+  const { base, quote } = useParams();
+  // const correctQuote = quote === "IRT" ? "USDT" : quote;
+  const correctQuote = () => {
+    if (quote === "IRT" && base != "USDT") {
+      return "USDT";
+    }
+    if (quote === "IRT" && base === "USDT") {
+      return "USD";
+    }
+    return quote;
+  };
+  console.log(correctQuote);
 
-const Chart = (props) => {
   return (
     <div className="custom-height h-[calc(100vh-64px)]">
       <AdvancedChart
         widgetProps={{
           theme: "dark",
-          symbol: `BINANCE:${props.symbol}`,
+          symbol: `${base}${correctQuote()}`,
           interval: "1H",
           height: "100%",
         }}
